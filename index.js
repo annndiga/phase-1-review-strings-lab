f0ca24d0fb64ed40a1f80a837b6e6a9a2e73790b
Ann Ndiga <annndiga@gmail.com>
2023-06-14 14:20:30 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Ann Ndiga';
+let wekcomeMessage = 'Welcome to Flatbook!';
+const welcomeMessage = 'Welcome to Flatbook, ${Ann Ndiga}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ${A .slice(0, 1)}!';
+
+
